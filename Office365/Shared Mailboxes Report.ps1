a0f64a78cc78ff6e2eae17fa8e64e692bdad2134
jstuart-tech <36063955+jstuart-tech@users.noreply.github.com>
2018-02-02 16:32:49 +1000

    Updated script
    Missing the @ symbol

--- Office365/Shared Mailboxes Report.ps1
+++ Office365/Shared Mailboxes Report.ps1
@@ -1,7 +1,7 @@
-$DomainName = "" #Enter Domainname here
+$DomainName = "" #Enter Domainname here e.g. contoso.com
 
 
-$DomainName = "*" + $DomainName
+$DomainName = "*@" + $DomainName
 $UserCredential = Get-Credential
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
 Import-PSSession $Session
