969c11985ff88f4526a5ea01ac4d2bda3f8e417a
jstuart-tech <36063955+jstuart-tech@users.noreply.github.com>
2018-02-02 16:31:33 +1000

    Office365
    Script to determine Office365 forwarding and access for Shared Mailboxes

---
+++ Office365/Shared Mailboxes Report.ps1
@@ -0,0 +1,26 @@
+$DomainName = "" #Enter Domainname here
+
+
+$DomainName = "*" + $DomainName
+$UserCredential = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+Import-PSSession $Session
+
+$SharedMailboxes = Get-Mailbox -Filter "*" | Where-Object {$_.RecipientTypeDetails -eq "SharedMailbox"} | Select-Object Name, ForwardingAddress, WindowsEmailAddress, alias
+
+ForEach($Mailbox in $SharedMailboxes)
+{
+    If($Mailbox.ForwardingAddress -ne $null)
+    {
+    Write-Host $Mailbox.Name "is forwarding to" $Mailbox.ForwardingAddress
+    }
+        $Perms = Get-MailboxPermission $Mailbox.alias | Where-Object {$_.User -like $DomainName}  
+            If($Perms.User -ne $null)
+            {
+            ForEach($User in $Perms.User)
+                {
+            $User2 = Get-Mailbox $User | Select-Object Name
+            Write-Host $User2.Name "has" $Perms.AccessRights[0] "to the mailbox of" $Mailbox.Name -ForegroundColor Red
+                }   
+            }
+}
